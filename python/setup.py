562dc3c4658dfe1fd0368423a94ee4d34ac0b722
Jeff Zemla <jeffzemla@gmail.com>
2018-02-26 15:12:08 -0600

    foo

--- python/setup.py
+++ python/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='jarjar',
-      version='2.0.1',
+      version='2.0.2',
       description='Programatically send messages to your slack team',
       url='https://github.com/AusterweilLab/jarjar',
       author='The Austerweil Lab at UW-Madison',
