53cdfae941d25956e3b2012b09ac9d8d6454cdc0
nolan <nolanbconaway@gmail.com>
2018-05-20 14:26:36 -0400

    draft venv test

--- setup.py
+++ setup.py
@@ -6,29 +6,29 @@ import re
 
 
 class OnlyGetScriptPath(install):
-    """Class to enable bin install location.
+	"""Class to enable bin install location.
 
-    see https://stackoverflow.com/a/25176606/353278
-    """
+	see https://stackoverflow.com/a/25176606/353278
+	"""
 
-    def run(self):
-        """Necessary method for setuptools."""
-        # does not call install.run() by design
-        self.distribution.install_scripts = self.install_scripts
+	def run(self):
+		"""Necessary method for setuptools."""
+		# does not call install.run() by design
+		self.distribution.install_scripts = self.install_scripts
 
 
 def get_setuptools_script_dir():
-    """Determine the script dir that setuptools would save in.
+	"""Determine the script dir that setuptools would save in.
 
-    see https://stackoverflow.com/a/25176606/353278
-    """
-    dist = Distribution({'cmdclass': {'install': OnlyGetScriptPath}})
-    dist.dry_run = True  # not sure if necessary, but to be safe
-    dist.parse_config_files()
-    command = dist.get_command_obj('install')
-    command.ensure_finalized()
-    command.run()
-    return dist.install_scripts
+	see https://stackoverflow.com/a/25176606/353278
+	"""
+	dist = Distribution({'cmdclass': {'install': OnlyGetScriptPath}})
+	dist.dry_run = True  # not sure if necessary, but to be safe
+	dist.parse_config_files()
+	command = dist.get_command_obj('install')
+	command.ensure_finalized()
+	command.run()
+	return dist.install_scripts
 
 
 jarjar_dir = get_setuptools_script_dir()
@@ -40,33 +40,29 @@ verstrline = open(VERSIONFILE, "rt").read()
 VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
 mo = re.search(VSRE, verstrline, re.M)
 if mo:
-    verstr = mo.group(1)
+	verstr = mo.group(1)
 else:
-    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
+	raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
 
 # install jarjar
 setup(
-    name='jarjar',
-    version=verstr,
-    description='Programatically send messages to your slack team',
-    url='https://github.com/AusterweilLab/jarjar',
-    author='The Austerweil Lab at UW-Madison',
-    author_email='austerweil.lab@gmail.com',
-    license='MIT',
-    keywords=['slack', 'messaging'],
-    packages=find_packages('.'),
-    install_requires=['requests>=2'],
-    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
-    zip_safe=False,
-    classifiers=[
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-    ],
-    data_files=[
-        (jarjar_dir, ['bin/jarjar'])
-    ]
+	name='jarjar',
+	version=verstr,
+	description='Programatically send messages to your slack team',
+	url='https://github.com/AusterweilLab/jarjar',
+	author='The Austerweil Lab at UW-Madison',
+	author_email='austerweil.lab@gmail.com',
+	license='MIT',
+	keywords=['slack', 'messaging'],
+	packages=find_packages('.'),
+	install_requires=['requests'],
+	python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
+	zip_safe=False,
+	classifiers=[
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3.4',
+		'Programming Language :: Python :: 3.5',
+		'Programming Language :: Python :: 3.6',
+	],
+	data_files=[(jarjar_dir, ['bin/jarjar'])]
 )
