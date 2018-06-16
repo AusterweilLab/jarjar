c2f498a015378b7787a401a4f6c4b7e4212955ee
nolan <nolanbconaway@gmail.com>
2018-06-16 16:03:02 -0400

    tabs to spaces

--- setup.py
+++ setup.py
@@ -10,29 +10,29 @@ verstrline = open(VERSIONFILE, "rt").read()
 VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
 mo = re.search(VSRE, verstrline, re.M)
 if mo:
-	verstr = mo.group(1)
+    verstr = mo.group(1)
 else:
-	raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
 
 # install jarjar
 setup(
-	name='jarjar',
-	version=verstr,
-	description='Programatically send messages to your slack team',
-	url='https://github.com/AusterweilLab/jarjar',
-	author='The Austerweil Lab at UW-Madison',
-	author_email='austerweil.lab@gmail.com',
-	license='MIT',
-	keywords=['slack', 'messaging'],
-	packages=find_packages('.'),
-	install_requires=['requests'],
-	python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
-	zip_safe=False,
-	classifiers=[
-		'Programming Language :: Python :: 2.7',
-		'Programming Language :: Python :: 3.4',
-		'Programming Language :: Python :: 3.5',
-		'Programming Language :: Python :: 3.6',
-	],
-	scripts=['bin/jarjar'],
+    name='jarjar',
+    version=verstr,
+    description='Programatically send messages to your slack team',
+    url='https://github.com/AusterweilLab/jarjar',
+    author='The Austerweil Lab at UW-Madison',
+    author_email='austerweil.lab@gmail.com',
+    license='MIT',
+    keywords=['slack', 'messaging'],
+    packages=find_packages('.'),
+    install_requires=['requests'],
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    scripts=['bin/jarjar'],
 )
