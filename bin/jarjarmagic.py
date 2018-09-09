b7865e2b7832845869e87a96cc2438cd7579faec
nolan <nolanbconaway@gmail.com>
2018-09-09 13:10:35 -0400

    cleanup

--- bin/jarjarmagic.py
+++
@@ -1,4 +0,0 @@
-from jarjar import jarjar
-from IPython.core.magic import register_line_magic
-from IPython.core.magic import register_cell_magic
-from IPython.core.magic import register_line_cell_magic
